a51a84a768f357117dda23c0cd5736450ebc2391
Шухрат Шадмонов <zumra6a@yandex.ru>
2023-07-03 23:22:00 +0300

    tests: mock private model maps

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -11,64 +11,63 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.ReflectionUtils;
 import ru.yandex.practicum.filmorate.model.Film;
 
+import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @WebMvcTest(controllers = FilmController.class)
 public class FilmControllerTest {
     private final String uri = "/films";
+    private Map<Integer, Film> films = new HashMap<>();
 
     @Autowired
     protected MockMvc mockMvc;
-
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private FilmController filmController;
 
     @BeforeEach
-    public void setup() throws Exception {
-        final LocalDate releaseDate = LocalDate.of(2023, 07, 01);
-        final Film film = new Film("film name", "film description", releaseDate, 120);
+    public void setup() {
+        Field field = ReflectionUtils.findField(FilmController.class, "films");
 
-        this.mockMvc
-                .perform(
-                        MockMvcRequestBuilders.post(uri)
-                                .content(objectMapper.writeValueAsString(film))
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .characterEncoding(StandardCharsets.UTF_8)
-                )
-                .andExpect(
-                        MockMvcResultMatchers
-                                .status()
-                                .isOk()
-                );
+        ReflectionUtils.makeAccessible(field);
+        ReflectionUtils.setField(field, filmController, films);
     }
 
     @AfterEach
     public void cleanup() throws Exception {
-        this.mockMvc
-                .perform(
-                        MockMvcRequestBuilders.delete(uri)
-                )
-                .andExpect(
-                        MockMvcResultMatchers
-                                .status()
-                                .isOk()
-                );
+        films.clear();
     }
 
     @Test
     @DisplayName("should return all films")
     public void testGetAllFilms() throws Exception {
+        final Film film = Film.builder()
+                .id(1)
+                .name("name")
+                .description("description")
+                .releaseDate(LocalDate.of(2023, 07, 01))
+                .duration(120)
+                .build();
+
+        films.put(film.getId(), film);
+
         this.mockMvc
                 .perform(
                         MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("film name"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].description").value("film description"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("name"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].description").value("description"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].releaseDate").value("2023-07-01"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].duration").value(120));
     }
@@ -76,33 +75,49 @@ public class FilmControllerTest {
     @Test
     @DisplayName("should update film")
     public void testUpdateFilm() throws Exception {
-        final LocalDate releaseDate = LocalDate.of(2023, 07, 01);
-        final Film film = new Film("name", "description", releaseDate, 120);
+        final Film film = Film.builder()
+                .id(1)
+                .name("name")
+                .description("description")
+                .releaseDate(LocalDate.of(2023, 07, 01))
+                .duration(120)
+                .build();
 
-        film.setId(1);
+        films.put(film.getId(), film);
+
+        final Film updatedFilm = film.toBuilder()
+                .description("new description")
+                .releaseDate(LocalDate.of(2023, 07, 02))
+                .build();
 
         this.mockMvc
                 .perform(
                         MockMvcRequestBuilders.put(uri)
-                                .content(objectMapper.writeValueAsString(film))
+                                .content(objectMapper.writeValueAsString(updatedFilm))
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .characterEncoding(StandardCharsets.UTF_8)
                 )
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$").isMap())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("name"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.description").value("description"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.releaseDate").value("2023-07-01"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.description").value("new description"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.releaseDate").value("2023-07-02"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.duration").value(120));
+
+        assertEquals(1, films.size());
+        assertEquals(updatedFilm, films.get(1));
     }
 
     @Test
     @DisplayName("should fail to update non existent film")
     public void testUpdateNonExistentFilm() throws Exception {
-        final LocalDate releaseDate = LocalDate.of(2023, 07, 01);
-        final Film film = new Film("name", "description", releaseDate, 120);
-
-        film.setId(1000);
+        final Film film = Film.builder()
+                .id(1000)
+                .name("name")
+                .description("description")
+                .releaseDate(LocalDate.of(2023, 07, 01))
+                .duration(120)
+                .build();
 
         this.mockMvc
                 .perform(
@@ -112,13 +127,19 @@ public class FilmControllerTest {
                                 .characterEncoding(StandardCharsets.UTF_8)
                 )
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        assertEquals(0, films.size());
     }
 
     @Test
     @DisplayName("should add to film")
     public void testAddFilm() throws Exception {
-        final LocalDate releaseDate = LocalDate.of(2023, 07, 01);
-        final Film film = new Film("New film name", "New film description", releaseDate, 120);
+        final Film film = Film.builder()
+                .name("New film name")
+                .description("New film description")
+                .releaseDate(LocalDate.of(2023, 8, 05))
+                .duration(65)
+                .build();
 
         this.mockMvc
                 .perform(
@@ -131,7 +152,9 @@ public class FilmControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$").isMap())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("New film name"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.description").value("New film description"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.releaseDate").value("2023-07-01"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.duration").value(120));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.releaseDate").value("2023-08-05"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.duration").value(65));
+
+        assertEquals(1, films.size());
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -11,56 +11,55 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.ReflectionUtils;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.lang.reflect.Field;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @WebMvcTest(controllers = UserController.class)
 public class UserControllerTest {
     private final String uri = "/users";
+    private Map<Integer, User> users = new HashMap<>();
 
     @Autowired
     protected MockMvc mockMvc;
-
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private UserController userController;
 
     @BeforeEach
-    public void setup() throws Exception {
-        final LocalDate birthday = LocalDate.of(2000, 07, 01);
-        final User user = new User("email@adress.com", "login", "User Name", birthday);
+    public void setup() {
+        Field field = ReflectionUtils.findField(UserController.class, "users");
 
-        this.mockMvc
-                .perform(
-                        MockMvcRequestBuilders.post(uri)
-                                .content(objectMapper.writeValueAsString(user))
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .characterEncoding(StandardCharsets.UTF_8)
-                )
-                .andExpect(
-                        MockMvcResultMatchers
-                                .status()
-                                .isOk()
-                );
+        ReflectionUtils.makeAccessible(field);
+        ReflectionUtils.setField(field, userController, users);
     }
 
     @AfterEach
     public void cleanup() throws Exception {
-        this.mockMvc
-                .perform(
-                        MockMvcRequestBuilders.delete(uri)
-                )
-                .andExpect(
-                        MockMvcResultMatchers
-                                .status()
-                                .isOk()
-                );
+        users.clear();
     }
 
     @Test
     @DisplayName("should return all users")
     public void testGetAllUsers() throws Exception {
+        final User user = User.builder()
+                .id(1)
+                .email("email@adress.com")
+                .login("login")
+                .name("User Name")
+                .birthday(LocalDate.of(2000, 07, 01))
+                .build();
+
+        users.put(user.getId(), user);
+
         this.mockMvc
                 .perform(
                         MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON)
@@ -76,15 +75,27 @@ public class UserControllerTest {
     @Test
     @DisplayName("should update user")
     public void testUpdateUser() throws Exception {
-        final LocalDate birthday = LocalDate.of(2000, 07, 01);
-        final User user = new User("new-email@adress.com", "new-login", "New Name", birthday);
+        final User user = User.builder()
+                .id(1)
+                .email("email@adress.com")
+                .login("login")
+                .name("User Name")
+                .birthday(LocalDate.of(2000, 07, 01))
+                .build();
 
-        user.setId(1);
+        users.put(user.getId(), user);
+
+        final User updatedUser = user.toBuilder()
+                .id(1)
+                .email("new-email@adress.com")
+                .login("new-login")
+                .name("New Name")
+                .build();
 
         this.mockMvc
                 .perform(
                         MockMvcRequestBuilders.put(uri)
-                                .content(objectMapper.writeValueAsString(user))
+                                .content(objectMapper.writeValueAsString(updatedUser))
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .characterEncoding(StandardCharsets.UTF_8)
                 )
@@ -94,15 +105,21 @@ public class UserControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.login").value("new-login"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("New Name"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.birthday").value("2000-07-01"));
+
+        assertEquals(1, users.size());
+        assertEquals(updatedUser, users.get(1));
     }
 
     @Test
     @DisplayName("should fail to update non existent user")
     public void testUpdateNonExistentUser() throws Exception {
-        final LocalDate birthday = LocalDate.of(2000, 07, 01);
-        final User user = new User("email@adress.com", "login", "User Name", birthday);
-
-        user.setId(1000);
+        final User user = User.builder()
+                .id(1000)
+                .email("email@adress.com")
+                .login("login")
+                .name("User Name")
+                .birthday(LocalDate.of(2000, 07, 01))
+                .build();
 
         this.mockMvc
                 .perform(
@@ -112,13 +129,19 @@ public class UserControllerTest {
                                 .characterEncoding(StandardCharsets.UTF_8)
                 )
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        assertEquals(0, users.size());
     }
 
     @Test
     @DisplayName("should add to user")
     public void testAddUser() throws Exception {
-        final LocalDate birthday = LocalDate.of(2000, 07, 01);
-        final User user = new User("email@adress.com", "login", "User Name", birthday);
+        final User user = User.builder()
+                .email("email@adress.com")
+                .login("login")
+                .name("User Name")
+                .birthday(LocalDate.of(2000, 07, 01))
+                .build();
 
         this.mockMvc
                 .perform(
@@ -133,5 +156,7 @@ public class UserControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.login").value("login"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("User Name"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.birthday").value("2000-07-01"));
+
+        assertEquals(1, users.size());
     }
 }
